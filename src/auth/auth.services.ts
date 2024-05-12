485c63eb0ff842afceb8f6bfa7b3085576a51896
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-05-12 20:05:31 -0300

    feat: create authorizer login

---
+++ src/auth/auth.services.ts
@@ -0,0 +1,49 @@
+import { User } from "../types";
+import { insertInTo, selectTable } from "../utils";
+import { AuthRequest, ResponseAuth } from "./auth.types";
+
+
+const jwt = require('jsonwebtoken');
+
+export function AuthenticatorUser(props: AuthRequest): ResponseAuth {
+    const { payload } = props;
+
+    if (payload?.email && payload?.password) {
+        const users = selectTable("users");
+
+        if (users) {
+            const user = users.find((user: User) => user.email == payload.email && user.password == payload.password);
+
+            if (user) {
+                const dataJwt = { userId: user.id, exp: Math.floor(Date.now() / 1000) + (60 * 60) };
+                const authToken = jwt.sign(dataJwt, 'secreto');
+                
+                insertInTo("auth", { authToken, user });
+
+                delete user.password
+
+                return {
+                    data: {
+                        user,
+                        authToken
+                    },
+                    statusCode: 200
+                }
+            }
+
+            return {
+                data: "not found",
+                statusCode: 404
+            }
+        }
+
+        return {
+            data: "internal db error",
+            statusCode: 500
+        }
+    }
+    return {
+        data: "payload is invalid",
+        statusCode: 400
+    }
+}
