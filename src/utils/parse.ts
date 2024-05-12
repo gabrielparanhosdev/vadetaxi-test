985a72d82832c878cb1ca75c026b152138e80d07
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-05-12 20:02:18 -0300

    feat: parseUrlParams for get urlBase

---
+++ src/utils/parse.ts
@@ -0,0 +1,38 @@
+/*
+    Aqui transformamos os dados da url em paramtros JSON, usando o baseUrl
+    Exemplo: 
+        baseUrl => /users/:userId
+        url => /users/87979
+        resposta => { 
+            params: {
+                userId: 87979
+            }
+        }
+*/
+
+import { ParamsObject } from "./types";
+
+export const parseUrlParams = (url: string, baseUrl: string) => {
+    const regex = /:([^/?]+)/g;
+    const paramNames = [];
+    let match;
+
+    while ((match = regex.exec(baseUrl)) !== null) {
+        paramNames.push(match[1]);
+    }
+
+    const dynamicRegex = new RegExp(baseUrl.replace(/\//g, '\\/').replace(regex, '([^/?]+)') + '$');
+    const matches = dynamicRegex.exec(url);
+
+    if (matches) {
+        const params: ParamsObject = {};
+
+        paramNames.forEach((paramName: string, index: number) => {
+            params[paramName] = matches[index + 1];
+        });
+        
+        return params;
+    }
+
+    return null;
+};
