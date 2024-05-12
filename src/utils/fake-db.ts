26761c02f30483132fe4600fe09f2b6fb1ea708b
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-05-12 20:01:07 -0300

    feat: create fakeDB for simulate get, insert, update and delete

---
+++ src/utils/fake-db.ts
@@ -0,0 +1,35 @@
+const NodeCache = require('node-cache');
+const cache = new NodeCache({ stdTTL: 0 });
+
+export function selectTable(tableName: string): any {
+    // aqui poderiamos fazer o select no banco de dados MySQL, MongoDB entre outros
+    return cache.get(tableName);
+}
+
+export function insertInTo(tableName: string, data: any): boolean {
+    // aqui podemos fazer o insert em um banco de daods também por exemplo.
+    cache.set(tableName, data);
+    return true;
+}
+
+export function updateTable(tableName: string, data: any, req: any, where: string): any {
+    // aqui poderiamos realizar um update mais simplificado usando uma lib de banco de dados
+
+    const table = selectTable(tableName);
+
+    if (table) {
+        const index = table.findIndex((line: any) => line[where] == req[where]);
+        if (index !== -1) {
+
+            delete data.id
+
+            table[index] = { ...table[index], ...data };
+            cache.set(tableName, table);
+            return table[index]
+        }
+
+        return null
+    }
+
+    return null
+}
