3351eb26118091f95161196eb42fd629d0964053
Gabriel Paranhos <biel.ferreira8@gmail.com>
2024-05-12 20:03:14 -0300

    feat: create global types

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+
+interface ResponseService {
+    statusCode: number;
+    data?: any;
+    error?: string;
+}
+
+interface User {
+    name: string;
+    email: string;
+    password: string;
+    id: string
+    // Adicione outros campos do usuário conforme necessário
+}
+interface UserRequest {
+    name?: string;
+    email?: string;
+    password?: string;
+    id?: string
+    // Adicione outros campos do usuário conforme necessário
+}
+
+
+export {
+    User,
+    UserRequest,
+    ResponseService,
+}
